a0c16bbb83ea8f4fa4a36b0e8605bb800f388ced
Jordan Delbar <jordandelbar@gmail.com>
2025-02-16 12:17:53 +0100

    feat: Implement predict function

--- server/src/model_service.rs
+++ server/src/model_service.rs
@@ -1,7 +1,7 @@
 use crate::ModelService;
-use crate::{ImageFrame, PredictionBatch};
+use crate::{BoundingBox, ImageFrame, PredictionBatch};
 use image::{imageops::FilterType, GenericImageView};
-use ndarray::{Array, Ix4};
+use ndarray::{s, Array, Axis, Ix4};
 use ort::session::{Session, SessionOutputs};
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
@@ -9,7 +9,28 @@ use std::sync::{
 };
 use tonic::{async_trait, Status};
 
-fn transform_image_frame(image_frame: &ImageFrame) -> Result<Array<f32, Ix4>, String> {
+#[rustfmt::skip]
+const YOLOV8_CLASS_LABELS: [&str; 80] = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+	"fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+	"bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+	"wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+	"carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet",
+	"tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+];
+
+fn intersection(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+    (box1.x2.min(box2.x2) - box1.x1.max(box2.x1)) * (box1.y2.min(box2.y2) - box1.y1.max(box2.y1))
+}
+
+fn union(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+    ((box1.x2 - box1.x1) * (box1.y2 - box1.y1)) + ((box2.x2 - box2.x1) * (box2.y2 - box2.y1))
+        - intersection(box1, box2)
+}
+
+fn transform_image_frame(image_frame: &ImageFrame) -> Result<(Array<f32, Ix4>, u32, u32), String> {
     let image_data = &image_frame.image_data;
 
     let image_reader = image::ImageReader::new(std::io::Cursor::new(image_data))
@@ -20,7 +41,7 @@ fn transform_image_frame(image_frame: &ImageFrame) -> Result<Array<f32, Ix4>, St
         .decode()
         .map_err(|e| format!("Error decoding image: {}", e))?;
 
-    let (_img_width, _img_height) = original_img.dimensions();
+    let (img_width, img_height) = original_img.dimensions();
     let img = original_img.resize_exact(640, 640, FilterType::CatmullRom);
 
     let mut input = Array::zeros((1, 3, 640, 640));
@@ -33,7 +54,7 @@ fn transform_image_frame(image_frame: &ImageFrame) -> Result<Array<f32, Ix4>, St
         input[[0, 2, y, x]] = (b as f32) / 255.;
     }
 
-    Ok(input)
+    Ok((input, img_height, img_width))
 }
 
 #[derive(Clone)]
@@ -70,8 +91,8 @@ impl OrtModelService {
 impl ModelService for OrtModelService {
     async fn predict(&self, frame: ImageFrame) -> Result<PredictionBatch, Status> {
         let input_result = transform_image_frame(&frame);
-        let input = match input_result {
-            Ok(input) => input,
+        let (input, img_height, img_width) = match input_result {
+            Ok(result) => result,
             Err(err) => {
                 return Err(Status::invalid_argument(format!(
                     "Image transformation error: {}",
@@ -86,8 +107,69 @@ impl ModelService for OrtModelService {
             Err(err) => return Err(err),
         };
 
-        // Placeholder
-        let prediction_batch = PredictionBatch::default();
+        let output = outputs["output0"]
+            .try_extract_tensor::<f32>()
+            .unwrap()
+            .t()
+            .into_owned();
+
+        let mut boxes = Vec::new();
+        let output = output.slice(s![.., .., 0]);
+
+        for row in output.axis_iter(Axis(0)) {
+            let row: Vec<_> = row.iter().copied().collect();
+            let (class_id, prob) = row
+                .iter()
+                .skip(4)
+                .enumerate()
+                .map(|(index, value)| (index, *value))
+                .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
+                .unwrap();
+
+            if prob < 0.5 {
+                continue;
+            }
+
+            let xc = row[0] / 640. * (img_width as f32);
+            let yc = row[1] / 640. * (img_height as f32);
+            let w = row[2] / 640. * (img_width as f32);
+            let h = row[3] / 640. * (img_height as f32);
+
+            boxes.push(BoundingBox {
+                x1: xc - w / 2.,
+                y1: yc - h / 2.,
+                x2: xc + w / 2.,
+                y2: yc + h / 2.,
+                class_label: YOLOV8_CLASS_LABELS[class_id].to_string(),
+                confidence: prob,
+            });
+        }
+
+        boxes.sort_by(|box1, box2| box2.confidence.total_cmp(&box1.confidence));
+        let mut result = Vec::new();
+
+        while !boxes.is_empty() {
+            result.push(boxes[0].clone());
+            boxes = boxes
+                .iter()
+                .filter(|box1| intersection(&boxes[0], &box1) / union(&boxes[0], &box1) < 0.7)
+                .cloned()
+                .collect();
+        }
+
+        let mut prediction_batch = PredictionBatch::default();
+        for bbox in result {
+            let prediction = BoundingBox {
+                class_label: bbox.class_label,
+                confidence: bbox.confidence,
+                x1: bbox.x1,
+                y1: bbox.y1,
+                x2: bbox.x2,
+                y2: bbox.y2,
+            };
+            prediction_batch.detections.push(prediction);
+        }
+
         Ok(prediction_batch)
     }
 }
@@ -115,8 +197,11 @@ mod tests {
 
         assert!(input_array_result.is_ok());
 
-        let input_array: Array<f32, Ix4> = input_array_result.unwrap();
+        let (input_array, img_height, img_width): (Array<f32, Ix4>, u32, u32) =
+            input_array_result.unwrap();
 
         assert_eq!(input_array.shape(), &[1, 3, 640, 640]);
+        assert_eq!(img_width, 100);
+        assert_eq!(img_height, 100);
     }
 }
