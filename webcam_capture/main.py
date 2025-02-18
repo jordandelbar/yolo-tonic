5c7d6b50c68dbf8e50267c93de7ab7a68283742a
Jordan Delbar <jordandelbar@gmail.com>
2025-02-18 09:07:08 +0100

    feat: Handling client disconnect

--- webcam_capture/main.py
+++ webcam_capture/main.py
@@ -16,7 +16,6 @@ last_prediction = None
 last_prediction_lock = threading.Lock()
 latest_frame = None
 frame_lock = threading.Lock()
-stop_prediction_request = threading.Event()
 
 # TODO: environment variable
 server_address = "localhost:50051"
@@ -25,7 +24,7 @@ stub = yolo_service_grpc.YoloServiceStub(channel)
 
 cap = cv2.VideoCapture(0)
 
-def send_prediction_request():
+def send_prediction_request(stop_prediction_request):
     global last_prediction
     logger.info("prediction thread started")
     while not stop_prediction_request.is_set():
@@ -61,52 +60,58 @@ async def video_feed(request: Request):
 
 async def generate_frames(request: Request):
     global latest_frame, last_prediction, cap
-    prediction_thread = threading.Thread(target=send_prediction_request, daemon=True)
-    prediction_thread.start()
-    try:
-        while True:
-            if cap is None:
-                time.sleep(0.1)
-                continue
+    while True:
+        stop_prediction_request = threading.Event()
+        prediction_thread = threading.Thread(target=send_prediction_request, args=(stop_prediction_request,), daemon=True)
+        prediction_thread.start()
 
-            if await request.is_disconnected():
-                logger.info("client disconnected. Stopping stream")
-                break
+        try:
+            while True:
+                if cap is None:
+                    time.sleep(0.1)
+                    continue
 
-            ret, frame = cap.read()
-            if not ret or frame is None:
-                logger.warning("failed to capture frame, skipping")
-                time.sleep(0.1)
-                continue
+                if await request.is_disconnected():
+                    logger.info("client disconnected. Stopping stream")
+                    break
 
-            with frame_lock:
-                latest_frame = frame.copy()
+                ret, frame = cap.read()
+                if not ret or frame is None:
+                    logger.warning("failed to capture frame, skipping")
+                    time.sleep(0.1)
+                    continue
 
-            with last_prediction_lock:
-                current_prediction = last_prediction
+                with frame_lock:
+                    latest_frame = frame.copy()
 
-            if current_prediction:
-                for detection in current_prediction:
-                    x1, y1, x2, y2 = map(int, [detection.x1, detection.y1, detection.x2, detection.y2])
-                    class_label, confidence = detection.class_label, detection.confidence
+                with last_prediction_lock:
+                    current_prediction = last_prediction
 
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cv2.putText(frame, str(class_label), (x1, max(20, y1 - 10)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                    cv2.putText(frame, f"{confidence:.2f}", (x1, min(frame.shape[0] - 10, y2 + 20)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                if current_prediction:
+                    for detection in current_prediction:
+                        x1, y1, x2, y2 = map(int, [detection.x1, detection.y1, detection.x2, detection.y2])
+                        class_label, confidence = detection.class_label, detection.confidence
 
-            ret, encoded_frame = cv2.imencode(".jpg", frame)
-            frame_bytes = encoded_frame.tobytes()
-            yield (b"--frame\r\n"
-                b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
-            time.sleep(0.033)
-    except Exception as e:
-        logger.error(f"error in generate frame: {e}")
-    finally:
-        stop_prediction_request.set()
-        prediction_thread.join()
-        logger.info("prediction thread stopped")
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        cv2.putText(frame, str(class_label), (x1, max(20, y1 - 10)),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                        cv2.putText(frame, f"{confidence:.2f}", (x1, min(frame.shape[0] - 10, y2 + 20)),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+                ret, encoded_frame = cv2.imencode(".jpg", frame)
+                frame_bytes = encoded_frame.tobytes()
+                yield (b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
+                time.sleep(0.033)
+        except Exception as e:
+            logger.error(f"error in generate frame: {e}")
+        finally:
+            stop_prediction_request.set()
+            prediction_thread.join()
+            logger.info("prediction thread stopped")
+        if await request.is_disconnected():
+            break
+            logger.info("restarting prediction thread for new stream")
 
 @app.on_event("startup")
 async def startup_event():
