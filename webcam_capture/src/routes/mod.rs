8c349813d35a710e751ea225c6fad02574bee7cc
Jordan Delbar <jordandelbar@gmail.com>
2025-03-04 08:54:23 +0100

    feat: Add router for routes

--- webcam_capture/src/routes/mod.rs
+++ webcam_capture/src/routes/mod.rs
@@ -1,5 +1,14 @@
+use crate::stream::VideoStream;
 mod health;
 mod video_feed;
 
-pub use health::healthcheck;
-pub use video_feed::video_feed;
+use health::healthcheck;
+use video_feed::video_feed;
+
+use axum::{routing::get, Router};
+
+pub fn api_routes() -> Router<VideoStream> {
+    Router::new()
+        .route("/video_feed", get(video_feed))
+        .route("/health", get(healthcheck))
+}
--- webcam_capture/src/server.rs
+++ webcam_capture/src/server.rs
@@ -1,10 +1,5 @@
-use crate::{
-    camera::Camera,
-    config::ServerConfig,
-    routes::{healthcheck, video_feed},
-    stream::VideoStream,
-};
-use axum::{routing::get, Router};
+use crate::{camera::Camera, config::ServerConfig, routes::api_routes, stream::VideoStream};
+use axum::Router;
 use std::sync::Arc;
 use tokio::{net::TcpListener, sync::broadcast::Receiver};
 
@@ -17,10 +12,7 @@ impl HttpServer {
     pub async fn new(camera: Arc<Camera>, server_config: &ServerConfig) -> anyhow::Result<Self> {
         let addr = server_config.get_address();
         let video_stream = VideoStream::new(camera, server_config.get_stream_delay_ms());
-        let router = Router::new()
-            .route("/video_feed", get(video_feed))
-            .route("/health", get(healthcheck))
-            .with_state(video_stream);
+        let router = Router::new().merge(api_routes()).with_state(video_stream);
 
         let listener = TcpListener::bind(addr).await?;
 
