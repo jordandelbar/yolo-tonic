4971560880a4db9fa1aa8831bcaeee7dc7ccf474
Jordan Delbar <35341015+jordandelbar@users.noreply.github.com>
2025-12-15 10:18:14 +0100

    chore: Update libaries versions (#2)
    * chore: Update libraries versions and git locks
    
    * chore: Reduce the number of fps to 30
    
    * fix: Fps computation
    
    * chore: Remove zune jpeg pin
    
    * docs: Update performances image

--- yolo_proto/build.rs
+++ yolo_proto/build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let descriptor_path = out_dir.join("yolo.bin");
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .file_descriptor_set_path(&descriptor_path)
         .compile_protos(&["proto/yolo_service.proto"], &["proto"])?;
